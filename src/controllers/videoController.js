a0135ab0b1007daca6b2a5d9e24324437c546ec9
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-01-03 13:57:03 +0900

    .

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,17 +1,22 @@
+import { response } from 'express'
 import Video from '../models/Video'
 
 export const home = async(req, res) => {
     try {
     const videos = await Video.find({})
-    console.log(videos)
+    // console.log(videos)
     return res.render('home', { pagetitle : "Home", videos}) 
     } catch {
         console.log('err')
     }
 }
-export const watch = (req, res) => {    
+export const watch = async (req, res) => {    
     const { id } = req.params
-    return res.render('watch', { pagetitle : `Watching`})
+    const video = await Video.findById(id)
+    if(video) {
+        return res.render('watch', { pagetitle : video.title, video})
+    }
+    return res.render('404', { pagetitle : 'Video not found.' })
 }
 export const getEdit = (req, res) => {
     const { id } = req.params
@@ -29,18 +34,32 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
     const { title, description, hashtags} = req.body
-    const video = new Video ({
-        title : title,
-        description : description,
-        createdAt : Date.now(),
-        hashtags : hashtags.split(',').map(word => `#${word}`),
-        meta : {
-            views : 0,  
-            rating : 0,
-        }
-    })
-    const dbVideo = await video.save() 
-    console.log(dbVideo)
-    return res.redirect('/')
+    // const video = new Video ({
+    //     title : title,
+    //     description : description,
+    //     createdAt : Date.now(),
+    //     hashtags : hashtags.split(',').map(word => `#${word}`),
+    //     meta : {
+    //         views : 0,  
+    //         rating : 0,
+    //     }
+    // })
+    // await video.save() 
+    
+    try {
+        await Video.create({
+            title : title,
+            description : description,
+            hashtags : hashtags.split(',').map(word => `#${word}`),
+        })
+        return res.redirect('/')
+    } catch(err) {
+        console.log(err)
+        return res.render('upload', {
+        pagetitle : 'Upload Video', 
+        errorMessage : err._message, 
+        })
+    }
+
      
 }
