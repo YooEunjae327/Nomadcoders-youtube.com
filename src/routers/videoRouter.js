25b9794280de833c8a76a15089871244e8f7ea27
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-01-05 21:08:14 +0900

    .

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,6 +6,6 @@ const videoRouter = express.Router()
 videoRouter.route('/upload').get(getUpload).post(postUpload)
 videoRouter.get('/:id', watch)
 videoRouter.route('/:id/edit').get(getEdit).post(postEdit)
+videoRouter.route
 
 export default videoRouter
-
