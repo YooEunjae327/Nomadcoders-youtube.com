bd57b37615f3a2eea60ef279e84756aefee2392b
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-01-07 20:29:30 +0900

    .

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import globalRouter from './routers/globalRouter'
+import rootRouter from './routers/rootRouter'
 import videoRouter from './routers/videoRouter'
 import userRouter from './routers/userRouter'
 
@@ -9,7 +9,7 @@ app.set('view engine', 'pug')
 app.set('views', './src/views')
 app.use(express.urlencoded({ extended : true}))
 
-app.use('/', globalRouter)
+app.use('/', rootRouter)
 app.use('/videos', videoRouter)
 app.use('/user', userRouter)
 
