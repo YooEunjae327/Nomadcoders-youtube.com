0a2614bb35f2129cec26171ceadcf6145293d900
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-01-09 20:03:03 +0900

    .

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,5 @@
 export const localsMiddleware = (req, res, next) => {
-    console.log(req.session)
     res.locals.loggedIn = Boolean(req.session.loggedIn)
-    // req.locals.stieName = 'Wetube'
-    console.log(res.locals)
+    res.locals.loggedInUser = req.session.user
     next()
 }
