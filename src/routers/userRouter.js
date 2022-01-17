de933cd1121899d2b5d78dfbf8acb8987b087e5b
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-01-17 20:11:24 +0900

    .

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -11,12 +11,12 @@ import {
     getChangePassword, 
     postChangePassword
 } from '../controllers/userController'
-import { protectedMiddleware, publicOnlyMiddleware, uploadFiles } from '../middlewares'
+import { avatarUpload, protectedMiddleware, publicOnlyMiddleware } from '../middlewares'
 
 const userRouter = express.Router()
 
 userRouter.get('/logout', protectedMiddleware, logout)
-userRouter.route('/edit').all(protectedMiddleware).get(getEdit).post(uploadFiles.single('avatar'), postEdit)
+userRouter.route('/edit').all(protectedMiddleware).get(getEdit).post(avatarUpload.single('avatar'), postEdit)
 userRouter.route('/change-password').all(protectedMiddleware).get(getChangePassword).post(postChangePassword)
 userRouter.get('/github/start', publicOnlyMiddleware, startGithubLogin)
 userRouter.get('/github/finish', publicOnlyMiddleware, finishGithubLogin)
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { getEdit,  watch, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController"
-import { protectedMiddleware } from "../middlewares"
+import { protectedMiddleware, videoUpload } from "../middlewares"
 
 const videoRouter = express.Router()
 
 videoRouter.route('/upload').all(protectedMiddleware).get(getUpload).post(postUpload)
 videoRouter.get('/:id', watch)
-videoRouter.route('/:id/edit').all(protectedMiddleware).get(getEdit).post(postEdit)
+videoRouter.route('/:id/edit').all(protectedMiddleware).get(getEdit).post(videoUpload.single('video'),postEdit)
 videoRouter.route('/:id/delete').all(protectedMiddleware).get(deleteVideo)
 
 
