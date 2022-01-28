c2267f93c47a2dd681120da5f20b599d4fbf3b26
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-01-28 13:56:07 +0900

    .

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -15,21 +15,33 @@ export const watch = async (req, res) => {
 }
 export const getEdit = async (req, res) => {
   const { id } = req.params
+  const {
+    user: { _id },
+  } = req.session
   const video = await Video.findById(id)
   if (!video) {
     return res.status(404).render("404", { pagetitle: "Video not found." })
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/")
   }
   return res.render("edit", { pagetitle: `Edit:${video.title}`, video })
 }
 export const postEdit = async (req, res) => {
   const { id } = req.params
   const { title, description, hashtags } = req.body
+  const {
+    user: { _id },
+  } = req.session
 
   const video = await Video.exists({ _id: id })
   //const video = await Video.findById(id)
 
   if (!video) {
     return res.render("404", { pagetitle: "Video not found." })
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/")
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -88,6 +100,17 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const { id } = req.params
+  const {
+    user: { _id },
+  } = req.session
+  const video = await Video.findById(id)
+
+  if (!video) {
+    return res.status(404).render("404", { pagetitle: "Video not found." })
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/")
+  }
   console.log(id)
   await Video.findByIdAndDelete(id)
   // delete video
