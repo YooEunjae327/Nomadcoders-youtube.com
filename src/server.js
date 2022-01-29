bc68762cda65d912dffed1e4b7cd2672e61a2084
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-01-29 22:26:27 +0900

    .

--- src/server.js
+++ src/server.js
@@ -1,35 +1,28 @@
-import express from 'express'
-import session from 'express-session'
-import MongoStore from 'connect-mongo'
-import rootRouter from './routers/rootRouter'
-import videoRouter from './routers/videoRouter'
-import userRouter from './routers/userRouter'
-import { localsMiddleware } from './middlewares'
+import express from "express"
+import session from "express-session"
+import MongoStore from "connect-mongo"
+import rootRouter from "./routers/rootRouter"
+import videoRouter from "./routers/videoRouter"
+import userRouter from "./routers/userRouter"
+import { localsMiddleware } from "./middlewares"
 
 const app = express()
 
-app.set('view engine', 'pug')
-app.set('views', './src/views')
-app.use(express.urlencoded({ extended : true}))
+app.set("view engine", "pug")
+app.set("views", "./src/views")
+app.use(express.urlencoded({ extended: true }))
 
 app.use(
-    session({
-        secret : process.env.COOKIE_SECRET,
-        resave : false,
-        saveUninitialized : false,
-        store : MongoStore.create({ mongoUrl : process.env.DB_URL })
-    })
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
 )
 
 app.use(localsMiddleware)
-app.use('/uploads', express.static('uploads'))
-app.use('/', rootRouter)
-app.use('/videos', videoRouter)
-app.use('/users', userRouter)
-
-export default app
- 
-
-
- 
-
+app.use("/uploads", express.static("uploads"))
+app.use("/", rootRouter)
+app.use("/videos", videoRouter)
+app.use("/users", userRouter)
