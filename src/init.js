151d07cfa01e4d37354a0a85426c8066c74a6f5a
aaaaaa <tomato6726@gmail.com>
2022-03-03 19:55:19 +0900

    .

--- src/init.js
+++ src/init.js
@@ -1,11 +1,10 @@
-import 'dotenv/config'
-import './db'
-import './models/Video'
-import './models/User'
-import app from './server'
-const PORT = 3000
+import "dotenv/config"
+import "./db"
+import "./models/Video"
+import "./models/User"
+import app from "./server"
+const PORT = 4000
 
 app.listen(PORT, () => {
-    console.log(`✅ Server running on port:${PORT}`)
+  console.log(`✅ Server running on port:${PORT}`)
 })
-
